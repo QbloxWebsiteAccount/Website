0a51e0c21a93e61212ad483c796b5c7b8c4ef476
Roland Branten <roland.branten@gmail.com>
2020-05-19 17:03:59 +0200

    add ContentBlock

--- src/pages/spi.js
+++ src/pages/spi.js
@@ -2,6 +2,7 @@
 import { graphql } from "gatsby";
 import React from "react";
 import Head from "../global-components/Layout/Head";
+import ContentBlock from "../micro-components/ContentBlock";
 // =========================
 
 export default function SPI({ data, path }) {
@@ -12,7 +13,7 @@ export default function SPI({ data, path }) {
         description="Page description goes here"
         keywords="content"
       />
-      {/* <ContentBlock content={data.sanityPages.content} path={path} /> */}
+      <ContentBlock content={data.sanityPages.content} path={path} />
     </>
   );
 }
